170e93a193c55e455e5fa6edaf722ff746c6aab5
Valentina <vale.gutierrezc@gmail.com>
2018-11-13 20:20:13 -0300

    add controller, root page, bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#one'
+  get 'pages/one'
+  get 'pages/two'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
